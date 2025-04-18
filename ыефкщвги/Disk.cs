66c52e305883ce712903d5c53f7bc835f1d19445
ClyZeR1 <sergeistarodyb@gmail.com>
2025-04-18 12:43:45 +0300

    Add files via upload

---
+++ ыефкщвги/Disk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_practicum
+{
+    class Disk:IShape
+    {
+        private int radius;
+        public int Radius { get => radius; set => radius = value; }
+        public Disk(int radius)
+        {
+            Radius = radius;
+        }
+        public double Perimeter()
+        {
+            return Math.PI * 2 * radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Круг: Стороны - радиус = {Radius}");
+            Console.WriteLine($"Периметр: {Perimeter():F2}");
+            Console.WriteLine($"Площадь: {Area():F2}");
+        }
+        public static Disk Create()
+        {
+            Console.Clear();
+            Console.Write("Введите радиус: ");
+            int radius = int.Parse(Console.ReadLine());
+            return new Disk(radius);
+        }
+    }
+}
---
+++ ыефкщвги/Triangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_practicum
+{
+    public class Triangle : IShape
+    {
+        private int side1;
+        private int side2;
+        private int side3;
+
+        public int Side1 { get => side1; set => side1 = value; }
+        public int Side2 { get => side2; set => side2 = value; }
+        public int Side3 { get => side3; set => side3 = value; }
+
+        public Triangle(int side1, int side2, int side3)
+        {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                throw new ArgumentException("Стороны должны быть положительными.");
+            }
+
+            if (!(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1))
+            {
+                throw new ArgumentException("Треугольник с такими сторонами не существует.");
+            }
+
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public double Perimeter()
+        {
+            return side1 + side2 + side3;
+        }
+
+        public double Area()
+        {
+            double p = (side1 + side2 + side3) / 2.0;
+            double area = Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+
+            return area;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Треугольник: Стороны - {side1}, {side2}, {side3}");
+            Console.WriteLine($"Периметр: {Perimeter():F2}");
+            Console.WriteLine($"Площадь: {Area():F2}");
+        }
+        public static Triangle Create()
+        {
+            Console.Clear();
+            Console.Write("Введите 1 сторону: ");
+            int side1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите 2 сторону: ");
+            int side2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите 3 сторону: ");
+            int side3 = int.Parse(Console.ReadLine());
+            return new Triangle(side1, side2,side3);
+        }
+    }
+}
